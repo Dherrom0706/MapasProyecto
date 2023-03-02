d2ab7afe303e25902b51e8c51947e4a10839a78c
Dherrom0706 <dionihernandezromera@gmail.com>
2023-03-02 22:41:51 +0100

    Second Commit -> Poly-lines with api auto

--- app/src/main/java/www/iesmurgi/mymap/MainActivity.kt
+++ app/src/main/java/www/iesmurgi/mymap/MainActivity.kt
@@ -6,6 +6,8 @@ import android.graphics.Color.blue
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -15,10 +17,21 @@ import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonClickListener,GoogleMap.OnMyLocationClickListener {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, OnMyLocationButtonClickListener,
+    GoogleMap.OnMyLocationClickListener {
 
     private lateinit var map: GoogleMap
+    private lateinit var btnCalculate: Button
+    var poly: Polyline? = null
+    private var start: String = ""
+    private var end: String = ""
 
     companion object {
         const val REQUEST_CODE_LOCATION = 0
@@ -28,7 +41,57 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonC
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnCalculate = findViewById(R.id.btnCalculateRoute)
+        btnCalculate.setOnClickListener {
+            start = ""
+            end = ""
+            poly?.remove()
+            poly = null
+            Toast.makeText(this,"Selecciona punto de origen y final",Toast.LENGTH_SHORT).show()
+            if (::map.isInitialized) {
+                map.setOnMapClickListener {
+                    if (start.isEmpty()) {
+                        //"23423424,234234234"
+                        start = "${it.longitude},${it.latitude}"
+                    } else if (end.isEmpty()) {
+                        end = "${it.longitude},${it.latitude}"
+                        createRoute()
+                    }
+                }
+            }
+        }
         createFragment()
+    }
+
+    private fun createRoute() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(ApiService::class.java)
+                .getRoute("5b3ce3597851110001cf6248e17ff4e3003c4e4eb9ba8de821591d8d", start, end)
+            if (call.isSuccessful) {
+                drawRoute(call.body())
+
+            } else {
+                Log.i("dioni","no")
+            }
+        }
+    }
+
+    private fun drawRoute(routeResponse: RouteResponse?) {
+        val polylineOptions = PolylineOptions()
+        routeResponse?.features?.first()?.geometry?.coordinates?.forEach {
+            //Longitud y latitud
+            polylineOptions.add(LatLng(it[1],it[0]))
+        }
+        runOnUiThread {
+            poly = map.addPolyline(polylineOptions)
+        }
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.openrouteservice.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 
     private fun createFragment() {
@@ -39,23 +102,23 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonC
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         //createMarker()
-        createPolylines()
+        //createPolylines()
         map.setOnMyLocationButtonClickListener(this)
         map.setOnMyLocationClickListener(this)
         enableLocation()
 
     }
 
-    private fun createPolylines(){
+    /*private fun createPolylines() {
         val polylineOptions = PolylineOptions()
-            .add(LatLng(40.419173113350965,-3.705976009368897))
-            .add(LatLng( 40.4150807746539, -3.706072568893432))
-            .add(LatLng( 40.41517062907432, -3.7012016773223873))
-            .add(LatLng( 40.41713105928677, -3.7037122249603267))
-            .add(LatLng( 40.41926296230622,  -3.701287508010864))
-            .add(LatLng( 40.419173113350965, -3.7048280239105225))
+            .add(LatLng(40.419173113350965, -3.705976009368897))
+            .add(LatLng(40.4150807746539, -3.706072568893432))
+            .add(LatLng(40.41517062907432, -3.7012016773223873))
+            .add(LatLng(40.41713105928677, -3.7037122249603267))
+            .add(LatLng(40.41926296230622, -3.701287508010864))
+            .add(LatLng(40.419173113350965, -3.7048280239105225))
             .width(15f)
-            .color(ContextCompat.getColor(this,R.color.kotlin))
+            .color(ContextCompat.getColor(this, R.color.kotlin))
 
         val polyline = map.addPolyline(polylineOptions)
 
@@ -72,14 +135,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonC
 
         //polyline.startCap = RoundCap()
         //polyline.endCap = CustomCap(BitmapDescriptorFactory.fromResource(poner imagen en bitmap aqui))
-    }
+    }*/
 
-    private fun changeColor(polyline: Polyline){
-        when((0..3).random()){
-            0->polyline.color = ContextCompat.getColor(this,R.color.red)
-            1->polyline.color = ContextCompat.getColor(this,R.color.yellow)
-            2->polyline.color = ContextCompat.getColor(this,R.color.green)
-            3->polyline.color = ContextCompat.getColor(this,R.color.blue)
+    private fun changeColor(polyline: Polyline) {
+        when ((0..3).random()) {
+            0 -> polyline.color = ContextCompat.getColor(this, R.color.red)
+            1 -> polyline.color = ContextCompat.getColor(this, R.color.yellow)
+            2 -> polyline.color = ContextCompat.getColor(this, R.color.green)
+            3 -> polyline.color = ContextCompat.getColor(this, R.color.blue)
         }
     }
 
@@ -130,13 +193,17 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonC
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode){
-            REQUEST_CODE_LOCATION-> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        when (requestCode) {
+            REQUEST_CODE_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 map.isMyLocationEnabled = true
-            }else{
-                Toast.makeText(this, "Para activar la localizacion ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Para activar la localizacion ve a ajustes y acepta los permisos",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-            else->{}
+            else -> {}
         }
 
     }
@@ -144,18 +211,22 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonC
     override fun onResumeFragments() {
         super.onResumeFragments()
         if (!::map.isInitialized) return //por si no ha dado tiempo para que se haga el mapa
-        if (!isLocationPermissionGranted()){
+        if (!isLocationPermissionGranted()) {
             map.isMyLocationEnabled = false
-            Toast.makeText(this, "Para activar la localizacion ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this,
+                "Para activar la localizacion ve a ajustes y acepta los permisos",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
     override fun onMyLocationButtonClick(): Boolean {
-        Toast.makeText(this,"Boton pulsado",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Boton pulsado", Toast.LENGTH_SHORT).show()
         return false //sio aqui retornamos false nos llevara a donde este nuestra localizacion, sino no nos llevará
     }
 
     override fun onMyLocationClick(p0: Location) {
-        Toast.makeText(this,"Estas en ${p0.latitude}, ${p0.longitude}",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Estas en ${p0.latitude}, ${p0.longitude}", Toast.LENGTH_SHORT).show()
     }
 }
